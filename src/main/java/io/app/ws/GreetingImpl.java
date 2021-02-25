453bfc6d40e2261a86f95fe48b5543100f179b0e
wh81752@bitio.org <whaefelinger@gmail.com>
2021-02-25 21:31:18 +0100

    GreetingImpl implements SEI

--- src/main/java/io/app/ws/GreetingImpl.java
+++ src/main/java/io/app/ws/GreetingImpl.java
@@ -1,54 +1,57 @@
-package io.app.soap;
-
-import java.util.Map;
-
-import javax.inject.Inject;
-import javax.jws.WebService;
+package io.app.ws;
 
 import io.app.backend.HelloWorldBackend;
-import io.app.ws.HelloWorld;
+import io.app.domain.User;
+import org.apache.cxf.annotations.SchemaValidation;
 import org.jboss.logging.Logger;
 
-import io.app.domain.User;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import java.util.Map;
 
 /**
  * Implementation of HelloWorld.
  */
+//@WebService(endpointInterface = "io.app.ws.HelloWorld")
+//@ApplicationScoped
+//@SchemaValidation
+//@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class GreetingImpl implements HelloWorld {
+    static final Logger logger = Logger.getLogger(GreetingImpl.class);
 
-//@WebService(/* endpointInterface = "io.app.soap.HelloWorld" */)
-public class GreetingImpl /* implements HelloWorld */ {
-  static final Logger logger = Logger.getLogger(GreetingImpl.class);
+    @Inject
+    HelloWorldBackend hwService;
 
-  @Inject
-  HelloWorldBackend hwService;
-
-  //@Override
-  public String hello() {
-    return hwService.hello();
-  }
-  //@Override
-  public String greet() {
-    return hwService.hello();
-  }
-
-  //@Override
-  public String sayHi(String text) {
-    return hwService.sayHi(text);
-  }
-
-  //@Override
-  public void addUser(User user) {
-    try {
-      logger.info("sayHiToUser:" + user);
-      hwService.addUser(user);
-    } catch (Throwable e) {
-      logger.info("**** error seen:" + e);
-      throw e;
+    //@Override
+    public String hello() {
+        return hwService.hello();
     }
-  }
 
-  //@Override
-  public Map<Integer, User> getUsers() {
-    return hwService.getUsers();
-  }
+    //@Override
+    public String greet() {
+        return hwService.hello();
+    }
+
+    //@Override
+    public String sayHi(String text) {
+        return hwService.sayHi(text);
+    }
+
+    //@Override
+    public void addUser(User user) {
+        try {
+            logger.info("addUser:" + user);
+            hwService.addUser(user);
+        } catch (Throwable e) {
+            logger.info("**** error seen:" + e);
+            throw e;
+        }
+    }
+
+    //@Override
+    public Map<Integer, User> getUsers() {
+        return hwService.getUsers();
+    }
 }
